9ab894c1ec0f657aa6782e673c76b35fe1ddf7f6
wanfeng <2694603524@qq.com>
2023-05-25 17:09:17 +0800

    拖拽

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,24 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import RefStudy from './components/RefStudy/RefStudy.vue';
 // import ReactiveStudy from './components/ReactiveStudy/ReactiveStudy.vue'
-import UsePinia from './components/UsePinia/UsePinia.vue'
+// import UsePinia from './components/UsePinia/UsePinia.vue'
+import VisualDrag from './components/VisualDrag/VisualDrag.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
+  </div> -->
   <!-- <HelloWorld msg="Vite + Vue" /> -->
   <!-- <RefStudy /> -->
   <!-- <ReactiveStudy /> -->
-  <UsePinia />
+  <!-- <UsePinia /> -->
+  <VisualDrag />
 </template>
 
 <style scoped>
---
+++ src/components/VisualDrag/VisualDrag.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div
+      class="contain"
+      @dragstart="dragstartEvent"
+      @dragenter="dragenterEvent"
+      @dragover="dragoverEvent"
+      @dragend="dragendEvent"
+      @drop="releaseDrog"
+    >
+      <div
+        :class="item.name"
+        :draggable="true"
+        v-for="item in list"
+        :key="item.id"
+      >
+        {{ item.name }}
+      </div>
+      <div data-effect="copy" class="targetBox">box2</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+
+type Drag = {
+  id: number;
+  name: string;
+};
+
+let list = reactive<Drag[]>([
+  {
+    id: 1,
+    name: "box1",
+  },
+  {
+    id: 2,
+    name: "box2",
+  },
+  {
+    id: 3,
+    name: "box3",
+  },
+]);
+
+let source;
+
+// 用户开始拖动元素时触发
+function dragstartEvent(e: any) {
+    // e.dataTransfer.effectAllowed= 'move'
+    console.log('dragstartEvent',e);
+    source = e.target // 获取目标节点
+}
+
+function dragendEvent(e: any) {}
+
+// 在一个拖动过程中，释放鼠标键时触发此事件
+function releaseDrog(e: any) {
+    console.log('releaseDrog', e);
+    // const copy = source.cloneNode(true)
+}
+
+// 当被鼠标拖动的对象进入其容器范围内时触发此事件
+function dragenterEvent(e: any) {
+  // e.preventDefault()
+}
+
+// 当某被拖动的对象在另一对象容器范围内拖动时触发此事件
+function dragoverEvent(e: any) {
+  e.preventDefault() // 阻止默认事件【解决元素不允许别的元素拖拽放在上面】
+}
+</script>
+
+<style scoped>
+.box1 {
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+}
+.box2 {
+  width: 100px;
+  height: 100px;
+  border: 1px solid rgb(0, 34, 255);
+}
+.box3 {
+  width: 100px;
+  height: 100px;
+  border: 1px solid rgb(0, 255, 110);
+}
+.targetBox {
+  width: 300px;
+  height: 300px;
+  border: 1px solid rgb(238, 0, 255);
+}
+</style>
